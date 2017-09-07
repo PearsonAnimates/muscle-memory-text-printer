4d29d596da9c9eae1636725b12dc75b1529b3ce3
Yael_Amaya <yiap2610@gmail.com>
2017-09-07 13:55:26 -0600

    Created Scanner to interact with computer

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,11 +1,25 @@
 package stupid.controller;
 
+import java.util.Scanner;
+
 public class StupidController
 {
 	public void start()
 	{
 		System.out.println("I hate this command so much!!! :D");
 		System.out.println("FRACK THIS COMMAND!!! I'm done, goodbye. :D");
+		
+		testScanner();
+	}
+	
+	@SuppressWarnings("resource")
+	private void testScanner()
+	{
+		Scanner firstScanner;
+		firstScanner = new Scanner(System.in);
+		System.out.println("What is your favorite food?");
+		String answer = firstScanner.next();
+		System.out.println("Cool, you like " + answer);
 	}
 
 }
