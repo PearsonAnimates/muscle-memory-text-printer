e5322b28d2344c5433875d3628977b563c73a930
Yael_Amaya <yiap2610@gmail.com>
2017-09-07 13:02:16 -0600

    All from memory Mr. Henrichsen
    Created text printer

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,11 @@
+package stupid.controller;
+
+public class StupidController
+{
+	public void start()
+	{
+		System.out.println("I hate this command so much!!! :D");
+		System.out.println("FRACK THIS COMMAND!!! I'm done, goodbye. :D");
+	}
+
+}
