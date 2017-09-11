48f18f835d5d8538d3f41e1e899af7922eb15cc3
Yael_Amaya <yiap2610@gmail.com>
2017-09-11 12:14:39 -0600

    I made a playdoh circle and printed it

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -2,24 +2,37 @@ package stupid.controller;
 
 import java.util.Scanner;
 
+import stupid.model.PlayDohCircle;
+
 public class StupidController
 {
+	public StupidController()
+	{
+		
+	}
+	
 	public void start()
 	{
 		System.out.println("I hate this command so much!!! :D");
 		System.out.println("FRACK THIS COMMAND!!! I'm done, goodbye. :D");
 		
 		testScanner();
+		PlayDohCircle firstCircle = new PlayDohCircle();
+		PlayDohCircle secondCircle;
+		secondCircle = new PlayDohCircle();
+		
+		System.out.println("Look I made a circle");
+		System.out.println(firstCircle);
+		
 	}
 	
-	@SuppressWarnings("resource")
 	private void testScanner()
 	{
 		Scanner firstScanner;
 		firstScanner = new Scanner(System.in);
 		System.out.println("What is your favorite food?");
 		String answer = firstScanner.next();
-		System.out.println("Cool, you like " + answer);
+		System.out.println("Cool, you like " + answer + " then? Well then, I don't like you, goodbye");
 	}
 
 }
---
+++ src/stupid/model/PlayDohCircle.java
@@ -0,0 +1,16 @@
+package stupid.model;
+
+import stupid.model.PlayDohCircle;
+
+public class PlayDohCircle
+{
+	//Declaration Section
+	//Data member Section
+	private int size;
+
+	public PlayDohCircle()
+	{
+		this.size = 5;
+		
+	}
+}
